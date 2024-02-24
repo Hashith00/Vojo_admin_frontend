da0ab25070fcb9c7782365a56e1d319c85bd77f2
Hashith Sithuruwan <mailtohashith@gmail.com>
2024-02-24 12:01:50 +0530

    changed the button text in rider details page

--- src/pages/riderDetails.jsx
+++ src/pages/riderDetails.jsx
@@ -49,7 +49,7 @@ function RiderDetails() {
             >
               <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
             </svg>
-            <span>Connect</span>
+            <span>Approve</span>
           </button>
           <button class="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100 ">
             <svg
@@ -64,7 +64,7 @@ function RiderDetails() {
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <span>Message</span>
+            <span>Remove</span>
           </button>
         </div>
       </div>
