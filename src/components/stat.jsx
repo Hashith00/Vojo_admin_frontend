eb8ac777a0aaeaefcc6ebe3b79a203012712406b
Taneesha <iyenshiaut@gmail.com>
2024-03-15 15:42:30 +0530

    adding backend part to statistics

--- src/components/stat.jsx
+++ src/components/stat.jsx
@@ -1,6 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+const Statistics = () => {
+  //Se the count to the variables
+  const [riderCount, setRiderCount] = useState(0);
+  const [hotelCount, setHotelCount] = useState(0);
+  const [usersCount, setUsersCount] = useState(0);
 
-function Statistics() {
+  useEffect(() => {
+    // Fetching the count of riders
+    const fetchRiderCount = async () => {
+      try {
+        const response = await axios.get('http://localhost:1000/api/rider/count');
+        const { val } = response.data; 
+        setRiderCount(val);
+        
+      } catch (error) {
+        console.error('Error fetching riders count:', error);
+      }
+    };
+
+    fetchRiderCount();
+
+    // Fetching the count of hotels
+    const fetchHotelCount = async () => {
+      try {
+        const response = await axios.get('http://localhost:1000/api/hotel/count');
+        const { val } = response.data; 
+        setHotelCount(val);
+        
+      } catch (error) {
+        console.error('Error fetching hotels count:', error);
+      }
+    };
+
+    fetchHotelCount();
+
+    // Fetching the count of users
+    const fetchUsersCount = async () => {
+      try {
+        const response = await axios.get('http://localhost:1000/api/user/count');
+        const { val } = response.data; // Extract the val from the response
+        setUsersCount(val);
+        
+      } catch (error) {
+        console.error('Error fetching users count:', error);
+      }
+    };
+
+    fetchUsersCount();
+  }, );
   return (
     <>
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
@@ -38,8 +86,7 @@ function Statistics() {
               </div>
 
               <h3 class="mt-2 text-2xl sm:text-3xl lg:text-4xl text-gray-800 dark:text-gray-800">
-                <span class="font-semibold">24</span>{" "}
-                <span class="text-gray-500">/ 100</span>
+                <span class="font-semibold">{usersCount}</span>{" "}
               </h3>
             </div>
           </div>
@@ -77,8 +124,7 @@ function Statistics() {
               </div>
 
               <h3 class="mt-2 text-2xl sm:text-3xl lg:text-4xl text-gray-800 dark:text-gray-800">
-                <span class="font-semibold">24</span>{" "}
-                <span class="text-gray-500">/ 100</span>
+                <span class="font-semibold">{riderCount}</span>{" "}
               </h3>
             </div>
           </div>
@@ -116,8 +162,7 @@ function Statistics() {
               </div>
 
               <h3 class="mt-2 text-2xl sm:text-3xl lg:text-4xl text-gray-800 dark:text-gray-800">
-                <span class="font-semibold">24</span>{" "}
-                <span class="text-gray-500">/ 100</span>
+                <span class="font-semibold">{hotelCount}</span>{" "}
               </h3>
             </div>
           </div>
