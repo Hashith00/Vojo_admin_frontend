347fdc8790350ee2f3dff146018f0244b8170d0d
Hashith Sithuruwan <88393998+Hashith00@users.noreply.github.com>
2024-06-29 11:07:36 +0530

    Update landingPage.jsx

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -40,7 +40,7 @@ function LandingPage() {
               <div className="mt-4 md:mb-12 max-w-2xl">
                 <h1 className="mb-4 font-semibold text-gray-800 text-4xl lg:text-5xl dark:text-gray-800">
                   See overall summary. Approve or delete users and service
-                  providers. Handle Payments.
+                  providers. Handle Payments and Journeys.
                 </h1>
                 <p className="text-red-600 dark:text-gray-400">Admin</p>
               </div>
